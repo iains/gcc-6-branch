a19dc7b0da7b753b9ce125d84326f4326b7a801a
Iain Sandoe <iain@sandoe.co.uk>
2019-11-01 20:05:17 +0000

    PPC: Handle GCC target pragma.
    For compatibility with other members of the port.
    Note, that we do not handle the longcall attribute, since longcall
    is not required/used on current Darwin.
    
    gcc/
    
    2020-03-08  Iain Sandoe  <iain@sandoe.co.uk>
    
    	Backport from mainline
    	2019-06-23  Iain Sandoe  <iain@sandoe.co.uk>
    
    	* config/rs6000/darwin.h: Handle GCC target pragma.

--- gcc/config/rs6000/darwin.h
+++ gcc/config/rs6000/darwin.h
@@ -382,6 +382,7 @@
   do \
     { \
       DARWIN_REGISTER_TARGET_PRAGMAS(); \
+      targetm.target_option.pragma_parse = rs6000_pragma_target_parse; \
       targetm.resolve_overloaded_builtin = altivec_resolve_overloaded_builtin; \
     } \
   while (0)
