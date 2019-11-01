0b453306098ae7cfb43fcf18db2f9ec48b797103
Iain Sandoe <iain@sandoe.co.uk>
2019-11-01 20:52:21 +0000

    machopic: Back out part of PR71767 fix.
    We applied a conservative, but fairly large, hammer to fix PR71767.
    However, ideally, we want minimise the number of symbols visible to
    ld64 and to match the cases emitted by clang (since that's what ld64
    is expecting). Now we've improved the handling of indirections, we
    can make the indirection symbols local when they are in the regular
    non-lazy symbol pointers section. We will continue to make any
    indirections in the data section visible (since right now we have no
    way to track if a given symbol follows a weak global).
    This change makes no difference to handling of labels for constants
    (to be revised in a future patch).
    
    There's a mechanical change to a number of tests (allowing 'l' or 'L'
    as the indirection symbol prefix).
    
    gcc/
    
    2020-03-08  Iain Sandoe  <iain@sandoe.co.uk>
    
    	Backport from mainline
    	2019-10-13  Iain Sandoe  <iain@sandoe.co.uk>
    
    	* config/darwin.c (machopic_indirection_name): Rework the
    	function to emit linker-visible symbols only for indirections
    	in the data section.  Clean up the code and update comments.
    
    gcc/testsuite/
    
    2020-03-08  Iain Sandoe  <iain@sandoe.co.uk>
    
    	Backport from mainline.
    	2019-10-13  Iain Sandoe  <iain@sandoe.co.uk>
    
    	* gcc.target/i386/indirect-thunk-1.c: Allow 'l' or 'L' in
    	indirection label prefix, for Darwin.
    	* gcc.target/i386/indirect-thunk-2.c: Likewise.
    	* gcc.target/i386/indirect-thunk-3.c: Likewise.
    	* gcc.target/i386/indirect-thunk-4.c: Likewise.
    	* gcc.target/i386/indirect-thunk-attr-1.c: Likewise.
    	* gcc.target/i386/indirect-thunk-attr-2.c: Likewise.
    	* gcc.target/i386/indirect-thunk-attr-3.c: Likewise.
    	* gcc.target/i386/indirect-thunk-attr-4.c: Likewise.
    	* gcc.target/i386/indirect-thunk-attr-5.c: Likewise.
    	* gcc.target/i386/indirect-thunk-attr-6.c: Likewise.
    	* gcc.target/i386/indirect-thunk-extern-1.c: Likewise.
    	* gcc.target/i386/indirect-thunk-extern-2.c: Likewise.
    	* gcc.target/i386/indirect-thunk-extern-3.c: Likewise.
    	* gcc.target/i386/indirect-thunk-extern-4.c: Likewise.
    	* gcc.target/i386/indirect-thunk-inline-1.c: Likewise.
    	* gcc.target/i386/indirect-thunk-inline-2.c: Likewise.
    	* gcc.target/i386/indirect-thunk-inline-3.c: Likewise.
    	* gcc.target/i386/indirect-thunk-inline-4.c: Likewise.
    	* gcc.target/i386/pr32219-2.c: Likewise.
    	* gcc.target/i386/pr32219-3.c: Likewise.
    	* gcc.target/i386/pr32219-4.c: Likewise.
    	* gcc.target/i386/pr32219-7.c: Likewise.
    	* gcc.target/i386/pr32219-8.c: Likewise.
    	* gcc.target/i386/ret-thunk-14.c: Likewise.
    	* gcc.target/i386/ret-thunk-15.c: Likewise.
    	* gcc.target/i386/ret-thunk-9.c: Likewise.

--- gcc/testsuite/gcc.target/i386/pr32219-2.c
+++ gcc/testsuite/gcc.target/i386/pr32219-2.c
@@ -13,5 +13,9 @@ foo ()
 
 /* { dg-final { scan-assembler-not "movl\[ \t\]xxx\\(%rip\\), %eax" { target { ! ia32 } } } } */
 /* { dg-final { scan-assembler "xxx@GOTPCREL" { target { ! ia32 } } } } */
-/* { dg-final { scan-assembler-not "movl\[ \t\]xxx@GOTOFF\\(%\[^,\]*\\), %eax" { target ia32 } } } */
-/* { dg-final { scan-assembler "movl\[ \t\]xxx@GOT\\(%\[^,\]*\\), %eax" { target ia32 } } } */
+
+/* { dg-final { scan-assembler-not "movl\[ \t\]xxx@GOTOFF\\(%\[^,\]*\\), %" { target { ia32 && { ! *-*-darwin* } } } } } */
+/* { dg-final { scan-assembler "movl\[ \t\]xxx@GOT\\(%\[^,\]*\\), %" { target { ia32 && { ! *-*-darwin* } } } } } */
+
+/* Darwin m32 defaults to PIC but common symbols need to be indirected.  */
+/* { dg-final { scan-assembler {movl[ \t][Ll]_xxx\$non_lazy_ptr-L1\$pb\(%eax\),[ \t]%eax} { target { ia32 && *-*-darwin* } } } } */
