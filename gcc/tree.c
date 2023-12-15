f91811406d814d0e2d5a63a75d0a2a2280f84a90
Iain Sandoe <iain@sandoe.co.uk>
2023-12-15 18:53:20 +0000

    java: Fix variant type TYPE_SIZE_UNIT mix-up.
    For some (probably historical) reason layout_class was trying to re-
    compute the TYPE_SIZE_UNIT as an int (rather than size-type).  This
    should (a) not be needed - since layout_type should compute the right
    value and (b) if it was done we should replicate the logic in
    layout_type for handling variant types and (c) it should be a sizetype
    anyway.
    
    Remove this and the FIXME in gcc/tree.c.

--- gcc/tree.c
+++ gcc/tree.c
@@ -13129,11 +13129,7 @@ verify_type_variant (const_tree t, tree tv)
     {
       verify_variant_match (TYPE_SIZE);
       verify_variant_match (TYPE_MODE);
-      if (TYPE_SIZE_UNIT (t) != TYPE_SIZE_UNIT (tv)
-	  /* FIXME: ideally we should compare pointer equality, but java FE
-	     produce variants where size is INTEGER_CST of different type (int
-	     wrt size_type) during libjava biuld.  */
-	  && !operand_equal_p (TYPE_SIZE_UNIT (t), TYPE_SIZE_UNIT (tv), 0))
+      if (TYPE_SIZE_UNIT (t) != TYPE_SIZE_UNIT (tv))
 	{
 	  error ("type variant has different TYPE_SIZE_UNIT");
 	  debug_tree (tv);
